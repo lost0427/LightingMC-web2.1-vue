e3168b25e6a623587973d48efee2c61ff3a98ae9
z1 <aaronzyg@163.com>
2023-11-04 22:24:29 +0800

    Gallery优化
    List！

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,143 +1,57 @@
 <script setup lang="ts">
-import img1 from "../assets/Gallery/2017summer.png";
-import img2 from "../assets/Gallery/2018summer.png";
-import img3 from "../assets/Gallery/Party1.png";
-import img4 from "../assets/Gallery/2022summer.png";
-import img5 from "../assets/Gallery/Airport1.png";
-import img6 from "../assets/Gallery/Airport2.png";
+import {
+  img1, img2, img3, img4, img5, img6, img7, img8, img9,
+  img10, img11, img12, img13, img14, img15, img16, img17, img18, img19
+} from "../assets/Gallery";
 
-import img7 from "../assets/Gallery/Bailu1.png";
-import img8 from "../assets/Gallery/Bailu2.png";
-import img9  from "../assets/Gallery/BBQ.png";
-import img10 from "../assets/Gallery/Party2.png";
-import img11 from "../assets/Gallery/Party3.png";
-import img12 from "../assets/Gallery/Groundhog1.png";
-import img13 from "../assets/Gallery/Groundhog2.png";
-import img14 from "../assets/Gallery/LDMC1.png";
-import img15 from "../assets/Gallery/LDMC2.png";
-import img16 from "../assets/Gallery/LDMC3.png";
-import img17 from "../assets/Gallery/Museum1.png";
-import img18 from "../assets/Gallery/LDMC4.png";
-import img19 from "../assets/Gallery/LDMC5.png";
+import { ref } from "vue";
+
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9,
+  img10, img11, img12, img13, img14, img15, img16, img17, img18, img19]; // 将图像引用存储在数组中
+const texts = [
+  '2017暑假合影纪念',
+  '2018暑假合影纪念',
+  '服务器最近一段时间举办的最盛大的派对活动，最高峰的参与人数达到11人',
+  '2022暑假合影',
+  '黄昏的灵动机场',
+  '灵动机场跑道起点',
+  '午后的白鹭公馆',
+  '白鹭公馆内部，Neboer的房间',
+  'Groundhog建造的烧烤亭',
+  '七周年派对准备点，Groundhog建造的，有蛋糕和多种木头。',
+  '银色派对活动中心，由xx建造，我们在这里度过了一段难忘的时光',
+  'Groundhog的家，很优雅的小楼',
+  'Groundhog的另一个家，用来吸烟的……是不是有点奇怪？',
+  '白天的灵动主岛，位于灵动主城的背面。',
+  '灵动MC的主城的俯瞰景色',
+  '黄昏的灵动主岛，也是首页配图的出处',
+  '灵动博物馆的入口，在白天的景色',
+  '灵动MC上一周目的首页配图',
+  'Krusl和Neboer在这里开启了灵动MC的最新一周目',
+]
+
+const list = ref<Array<{ id: string; text: string; img: any }>>([]);
+
+for (let i = 1; i <= 19; i++) {
+  list.value.push({
+    id: i.toString().padStart(3, '0'),
+    text: texts[i % texts.length],
+    img: images[i % images.length]
+  });
+}
+
 </script>
 
 <template>
   <div class="G flex-cc">
     <h1>灵动MC风景画廊</h1>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img1>
+    <d-card v-for="item in list" :key="item.id" v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src="item.img">
 
       <template #content>
-        2017暑假合影纪念
+        {{ item.text }}
       </template>
     </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img2>
 
-      <template #content>
-        2018暑假合影纪念
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img3>
-
-      <template #content>
-        服务器最近一段时间举办的最盛大的派对活动，最高峰的参与人数达到11人
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img4>
-
-      <template #content>
-        2022暑假合影
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img5>
-
-      <template #content>
-        黄昏的灵动机场
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img6>
-
-      <template #content>
-        灵动机场跑道起点
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img7>
-
-      <template #content>
-        午后的白鹭公馆
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img8>
-
-      <template #content>
-        白鹭公馆内部，Neboer的房间
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img9>
-
-      <template #content>
-        Groundhog建造的烧烤亭
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img10>
-
-      <template #content>
-        七周年派对准备点，Groundhog建造的，有蛋糕和多种木头。
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img11>
-
-      <template #content>
-        银色派对活动中心，由xx建造，我们在这里度过了一段难忘的时光
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img12>
-
-      <template #content>
-        Groundhog的家，很优雅的小楼
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img13>
-
-      <template #content>
-        Groundhog的另一个家，用来吸烟的……是不是有点奇怪？
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img14>
-
-      <template #content>
-        白天的灵动主岛，位于灵动主城的背面。
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img15>
-
-      <template #content>
-        灵动MC的主城的俯瞰景色
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img16>
-
-      <template #content>
-        黄昏的灵动主岛，也是首页配图的出处
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img17>
-
-      <template #content>
-        灵动博物馆的入口，在白天的景色
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img18>
-
-      <template #content>
-        灵动MC上一周目的首页配图
-      </template>
-    </d-card>
-    <d-card v-d-image-preview v-ripple="{ duration: 300 }" class="demo-card" :src=img19>
-
-      <template #content>
-        Krusl和Neboer在这里开启了灵动MC的最新一周目
-      </template>
-    </d-card>
   </div>
   <div style="height: 60px"></div>
 </template>
